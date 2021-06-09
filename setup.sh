cc935bd82081d0e0dcc136ad643f6da6702bacb9
Sanjeev Mallik <smallik123@users.noreply.github.com>
2021-06-09 17:06:17 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
- 
+docker network create --driver bridge intranet
 docker build -t webserver . --rm
+
+docker run --name mysql-57 -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
+cd /home/ubuntu
+git clone git@github.com:smallik123/SuiteCRM.git
+
+sudo mkdir logs
+
+docker run --name "localhost" -d -e VIRTUAL_HOST=localhost --add-host="localhost:127.0.0.1" -p 80:80 -v /home/ubuntu/:/var/log/apache2 -v /home/ubuntu/SuiteCRM:/var/www/html --net intranet webserver
+
+docker start $(docker ps -a -q -f status=exited)
