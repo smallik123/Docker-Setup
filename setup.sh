1ab3f372105e9e00a36310062cc7f54ff9ea7fe4
Sanjeev Mallik <smallik123@users.noreply.github.com>
2021-06-09 17:12:40 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ docker run --name mysql -e MYSQL_ROOT_PASSWORD=root -d mysql:5.7
 cd /home/ubuntu
 git clone git@github.com:smallik123/SuiteCRM.git
 
+sudo chmod -R 755 SuiteCRM
+
 sudo mkdir logs
 
 docker run --name "localhost" -d -e VIRTUAL_HOST=localhost --add-host="localhost:127.0.0.1" -p 80:80 -v /home/ubuntu/:/var/log/apache2 -v /home/ubuntu/SuiteCRM:/var/www/html --net intranet webserver
